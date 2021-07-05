e516d5cad5c062e7f7e6888768bf8104d13c75fe
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-07-05 17:16:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import torchvision
-from dvutils.dataloader.load_data import Cifar10DataLoader
+from dvutils.dataloader.load_data import *
 import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
-from torch.optim.lr_scheduler import StepLR,OneCycleLR
+from torch.optim.lr_scheduler import StepLR,OneCycleLR,ReduceLROnPlateau
 from dvutils.utils import train as trn
 from dvutils.utils import test as tst
 from torchsummary import summary
@@ -14,12 +14,14 @@ import numpy as np
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
+from torch_lr_finder import LRFinder
 
 
 class TriggerEngine:
     def __init__(self, config):
         self.config = config
-        self.cifar_dataset=Cifar10DataLoader(self.config)
+        self.loader = config['data_loader']['type']
+        self.cifar_dataset=eval(self.loader)(self.config)
         self.device = self.set_device()
         self.class_names = self.config['data_loader']['classes']
         self.writer = SummaryWriter()
@@ -36,7 +38,7 @@ class TriggerEngine:
         device = torch.device("cuda" if use_cuda else "cpu")
         return device
         
-    def run_experiment(self,model,train_loader,test_loader):
+    def run_experiment(self,model,train_loader,test_loader,best_lr):
         
         model.to(self.device) 
         dropout=self.config['model_params']['dropout']
@@ -54,13 +56,21 @@ class TriggerEngine:
         test_losses = []
         train_accuracy = []
         test_accuracy = []
+        plot_train_acc=[]
         lrs=[]
             
         
         #optimizer = optim.SGD(model.parameters(), lr=0.02, momentum=0.7,weight_decay=l2_factor)
         optimizer = opt_func(model.parameters(), lr=lr, weight_decay=l2_factor)
-        #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True,mode='max')
-        scheduler = OneCycleLR(optimizer, max_lr=lr,epochs=epochs,steps_per_epoch=len(train_loader))
+        
+        if self.config['lr_scheduler'] == 'OneCycleLR': 
+            print("using OneCycleLR")
+            scheduler = OneCycleLR(optimizer=optimizer, max_lr=best_lr,
+                                  epochs=epochs, steps_per_epoch=len(train_loader),
+                                  pct_start=5/epochs, div_factor=10, final_div_factor=10)
+            #scheduler = OneCycleLR(optimizer, max_lr=lr,epochs=epochs,steps_per_epoch=len(train_loader))
+        else:
+            scheduler = ReduceLROnPlateau(optimizer, factor=0.2, patience=3,verbose=True,mode='max')
 
         for epoch in range(1, epochs + 1):
             print(f'Epoch {epoch}:')
@@ -71,12 +81,28 @@ class TriggerEngine:
             self.writer.add_scalar('Epoch/Test/test_loss', test_losses[-1], epoch)
             self.writer.add_scalar('Epoch/Train/train_accuracy', train_accuracy[-1], epoch)
             self.writer.add_scalar('Epoch/Train/test_accuracy', test_accuracy[-1], epoch)
+            plot_train_acc.append(train_accuracy[-1])
 
-            # if epoch > 20:
-            #     scheduler.step(test_accuracy[-1])
+            if "ReduceLROnPlateau" in str(scheduler):
+                scheduler.step(test_accuracy[-1])
 
             self.writer.flush()
-        return (train_accuracy,train_losses,test_accuracy,test_losses)
+        return (plot_train_acc,train_losses,test_accuracy,test_losses)
+    
+    def find_lr(self,model,train_loader, test_loader, start_lr, end_lr, num_iterations):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(model.parameters(), lr=start_lr, momentum=0.90)
+        #optimizer = optim.Adam(model.parameters(), lr=0.1, weight_decay=1e-2)
+        lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
+        lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=end_lr, num_iter=num_iterations, step_mode="linear")
+        
+        # Plot
+        best_lr = lr_finder.plot(suggest_lr=True,skip_start=0, skip_end=0)
+
+        # Reset graph
+        lr_finder.reset()
+        return best_lr[1]
+    
         
     def save_experiment(self,model, experiment_name,path):
         print(f"Saving the model for {experiment_name}")
@@ -114,7 +140,7 @@ class TriggerEngine:
         fig.tight_layout()
         mean,std = self.cifar_dataset.calculate_mean_std()
         #mean,std = helper.calculate_mean_std("CIFAR10")
-        for i, (img, pred, correct) in enumerate(wrong_predictions[:10]):
+        for i, (img, pred, correct) in enumerate(wrong_predictions[:20]):
             img, pred, target = img.cpu().numpy().astype(dtype=np.float32), pred.cpu(), correct.cpu()
             for j in range(img.shape[0]):
                 img[j] = (img[j]*std[j])+mean[j]
